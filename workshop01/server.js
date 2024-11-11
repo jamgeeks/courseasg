db2acc59b0337889b7884d1def36bc61bec0ac10
jamie cheng <jamie_cheng@tech.gov.sg>
2024-11-12 00:13:40 +0800

    update the script and the port number

--- workshop01/server.js
+++ workshop01/server.js
@@ -33,7 +33,7 @@ app.get('/', (req, res) => {
 });
 
 // Start the server
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on port ${port}`);
 });
